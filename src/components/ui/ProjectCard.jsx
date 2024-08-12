e371482d1376566bafe68472c01f3a9f3bf48687
Rodrigo-Lajas <rodrigolajas18@gmail.com>
2024-08-12 16:58:46 -0300

    cambio diseño tarjetas

--- src/components/ui/ProjectCard.jsx
+++ src/components/ui/ProjectCard.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
 const ProjectCard = ({title, description, imageUrl, link}) => {
     return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-gray-600">
             <img className="w-full" src={imageUrl} alt={title} />
             <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">{title}</div> 
-                <p className="text-gray-700 text-base">{description}</p> 
+                <div className="font-bold text-xl mb-2 text-white">{title}</div> 
+                <p className="text-slate-300 text-base">{description}</p> 
                 </div> 
                 <div className="px-6 pt-4 pb-2">
                     <a href={link} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Ir al proyecto</a>
