d67ea4d7b4e388ab3f1756a24643cf4bfacd5dbb
Rodrigo-Lajas <rodrigolajas18@gmail.com>
2024-08-18 19:35:09 -0300

    fix link imagenes

--- src/components/ProjectsArray.jsx
+++ src/components/ProjectsArray.jsx
@@ -1,9 +1,12 @@
+import houstonguitarshop from "../assets/houstonguitarshop.png";
+import tiendadeguitarras from "../assets/tiendadeguitarras.png";
+
 const Proyects = [
     {
         id: 1,
         title: "Houston guitar shop",
         description: "Proyecto basado en una tienda ficticia que ofrece instrumentos musicales, accesorios, y cursos de guitarra de distintos niveles de dificultad.",
-        imageUrl: "../assets/houstonguitarshop.png",
+        imageUrl: houstonguitarshop,
         link: "https://houstonguitarshop.vercel.app/",
         repositorio: "https://github.com/Rodrigo-Lajas/PF-Lajas",
         tags: ["HTML", "CSS", "SASS", "Bootstrap"],
@@ -12,7 +15,7 @@ const Proyects = [
         id: 2,
         title: "Tienda de guitarras",
         description: "Proyecto creado a partir de una tienda ficticia de guitarras y accesorios. Se hace uso de carrito, sus funciones, y de filtro según productos o categorías.",
-        imageUrl: "../assets/tiendadeguitarras.png",
+        imageUrl: tiendadeguitarras,
         link: "https://rodrigo-lajas.github.io/ProyectoFinal-Lajas/",
         repositorio: "https://github.com/Rodrigo-Lajas/ProyectoFinal-Lajas",
         tags: ["HTML", "CSS", "JavaScript", "Bootstrap"],
