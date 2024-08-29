65ae10504afcaf4a6a16dd50a51418b732d3dbfc
Rodrigo-Lajas <rodrigolajas18@gmail.com>
2024-08-29 10:57:28 -0300

    google cloud storage

--- src/components/ProjectsArray.jsx
+++ src/components/ProjectsArray.jsx
@@ -1,10 +1,9 @@
-
 const Projects = [
     {
         id: 1,
         title: "Houston guitar shop",
         description: "Proyecto basado en una tienda ficticia que ofrece instrumentos musicales, accesorios, y cursos de guitarra de distintos niveles de dificultad.",
-        imageUrl: '../assets/houstonGuitarShop.png',
+        imageUrl: 'https://storage.cloud.google.com/portfolio_rodrigolajas/houstonGuitarShop.png',
         link: "https://houstonguitarshop.vercel.app/",
         tags: ["HTML", "CSS", "SASS", "Bootstrap"],
     },
@@ -12,7 +11,7 @@ const Projects = [
         id: 2,
         title: "Tienda de guitarras",
         description: "Proyecto creado a partir de una tienda ficticia de guitarras y accesorios. Se hace uso de carrito, sus funciones, y de filtro según productos o categorías.",
-        imageUrl: '../assets/tiendaDeGuitarras.png',
+        imageUrl: 'https://storage.cloud.google.com/portfolio_rodrigolajas/tiendaDeGuitarras.png',
         link: "https://rodrigo-lajas.github.io/ProyectoFinal-Lajas/",
         tags: ["HTML", "CSS", "JavaScript", "Bootstrap"],
     },
@@ -20,7 +19,7 @@ const Projects = [
         id: 3,
         title: "Tienda de telefonos celulares",
         description: "Proyecto creado a partir de una tienda ficticia de celulares. Se hace uso de carrito, sus funciones, y de filtro por categorías.",
-        imageUrl: '../assets/tiendaDeCelulares.png',
+        imageUrl: 'https://storage.cloud.google.com/portfolio_rodrigolajas/tiendaDeTelefonosCelulares.png',  
         link: "https://tienda-de-telefonos-celulares.vercel.app/",
         tags: ["HTML", "CSS", "JavaScript", "React", "React Bootstrap", "Firebase",],
     },
