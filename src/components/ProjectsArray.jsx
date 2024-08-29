17e1d6f273fbf297ce360b2c9742809d14722a8a
Rodrigo-Lajas <rodrigolajas18@gmail.com>
2024-08-29 09:24:32 -0300

    comment array

--- src/components/ProjectsArray.jsx
+++ src/components/ProjectsArray.jsx
@@ -1,31 +1,31 @@
-import houstonguitarshop from '../assets/houstonGuitarShop.png';
-import tiendaDeGuitarras from '../assets/tiendaDeGuitarras.png';
-import tiendaDeCelulares from '../assets/tiendaDeTelefonosCelulares.png';
-const Proyects = [
-    {
-        id: 1,
-        title: "Houston guitar shop",
-        description: "Proyecto basado en una tienda ficticia que ofrece instrumentos musicales, accesorios, y cursos de guitarra de distintos niveles de dificultad.",
-        imageUrl: houstonguitarshop,
-        link: "https://houstonguitarshop.vercel.app/",
-        tags: ["HTML", "CSS", "SASS", "Bootstrap"],
-    },
-    {
-        id: 2,
-        title: "Tienda de guitarras",
-        description: "Proyecto creado a partir de una tienda ficticia de guitarras y accesorios. Se hace uso de carrito, sus funciones, y de filtro según productos o categorías.",
-        imageUrl: tiendaDeGuitarras,
-        link: "https://rodrigo-lajas.github.io/ProyectoFinal-Lajas/",
-        tags: ["HTML", "CSS", "JavaScript", "Bootstrap"],
-    },
-    {
-        id: 3,
-        title: "Tienda de telefonos celulares",
-        description: "Proyecto creado a partir de una tienda ficticia de celulares. Se hace uso de carrito, sus funciones, y de filtro por categorías.",
-        imageUrl: tiendaDeCelulares,
-        link: "https://tienda-de-telefonos-celulares.vercel.app/",
-        tags: ["HTML", "CSS", "JavaScript", "React", "React Bootstrap", "Firebase",],
-    },
-]
+// import houstonguitarshop from '../assets/houstonGuitarShop.png';
+// import tiendaDeGuitarras from '../assets/tiendaDeGuitarras.png';
+// import tiendaDeCelulares from '../assets/tiendaDeTelefonosCelulares.png';
+// const Proyects = [
+//     {
+//         id: 1,
+//         title: "Houston guitar shop",
+//         description: "Proyecto basado en una tienda ficticia que ofrece instrumentos musicales, accesorios, y cursos de guitarra de distintos niveles de dificultad.",
+//         imageUrl: houstonguitarshop,
+//         link: "https://houstonguitarshop.vercel.app/",
+//         tags: ["HTML", "CSS", "SASS", "Bootstrap"],
+//     },
+//     {
+//         id: 2,
+//         title: "Tienda de guitarras",
+//         description: "Proyecto creado a partir de una tienda ficticia de guitarras y accesorios. Se hace uso de carrito, sus funciones, y de filtro según productos o categorías.",
+//         imageUrl: tiendaDeGuitarras,
+//         link: "https://rodrigo-lajas.github.io/ProyectoFinal-Lajas/",
+//         tags: ["HTML", "CSS", "JavaScript", "Bootstrap"],
+//     },
+//     {
+//         id: 3,
+//         title: "Tienda de telefonos celulares",
+//         description: "Proyecto creado a partir de una tienda ficticia de celulares. Se hace uso de carrito, sus funciones, y de filtro por categorías.",
+//         imageUrl: tiendaDeCelulares,
+//         link: "https://tienda-de-telefonos-celulares.vercel.app/",
+//         tags: ["HTML", "CSS", "JavaScript", "React", "React Bootstrap", "Firebase",],
+//     },
+// ]
 
-export default Proyects;
+// export default Proyects;
