2f239787cc18a13ece60b7f591b6142fb9b2727f
Rodrigo-Lajas <rodrigolajas18@gmail.com>
2024-08-29 00:10:40 -0300

    add speedInsights

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 export default function App() {
     return (
         <div className="flex flex-col h-screen font-sans">
@@ -13,6 +14,7 @@ export default function App() {
             <Projects />
             <Contact />
             <Footer />
+            <SpeedInsights />
         </div>
     );
 }
