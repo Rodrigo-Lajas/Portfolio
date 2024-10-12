267b4d0567e59400a438acbec7e045c8c5e7ab7a
Rodrigo-Lajas <rodrigolajas18@gmail.com>
2024-10-12 20:06:57 -0300

    borrado de boton volver arriba

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,6 @@ export default function Footer() {
     return (
         <footer className="bg-gray-800 text-white p-4 text-center">
             <p className="flex items-center m-4 text-lg leading-normal text-slate-300 font-noto-sans m-8">&copy; 2024 Rodrigo Lajas. Todos los derechos reservados.</p>
-            <a className="hover:text-lime-400 active:text-lime-600 transition duration-300 ease-in-out">Volver arriba</a>
         </footer>
     );
 }
