7c9e1ff8ec45b9040c26c061ec9e30924e2a5a79
Rodrigo-Lajas <rodrigolajas18@gmail.com>
2024-10-13 17:49:33 -0300

    cambio de seccion contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,27 +4,49 @@ import mail from '../assets/mail-svgrepo-com.svg';
 
 export default function Contacto() {
 
-    const contactArray = [
-        { id: 1, name: "GitHub", link: "https://github.com/Rodrigo-Lajas", logo: github },
-        { id: 2, name: "LinkedIn", link: "https://www.linkedin.com/in/rodrigolajas/", logo: linkedin },
-        { id: 3, name: "Mail", link: "mailto:rodrigolajas18@gmail.com", logo: mail },
-    ]
+    //     { id: 2, name: "LinkedIn", link: "https://www.linkedin.com/in/rodrigolajas/", logo: linkedin },
+    //     { id: 3, name: "Mail", link: "mailto:rodrigolajas18@gmail.com", logo: mail },
 
     return (
-        <section id="contact" className="bg-gray-800 text-white p-4 pt-12 my-auto min-h-screen">
-            <h3 className="flex items-center justify-center text-4xl leading-normal font-bold font-noto-sans w-auto m-8 bg-clip-text text-transparent bg-gradient-to-r from-lime-400 to-lime-100 sm:text-6xl sm:leading-normal">
-                Contacto
-            </h3>
-            
-            <div className="flex items-center justify-center mt-8">
-                {contactArray.map((contact) => (                   
-                <div key={contact.id}>
-                    <a href={contact.link} target="_blank" rel="noopener noreferrer" className="transition duration-300 ease-in-out">
-                        <img src={contact.logo} alt={contact.name} className="w-24 h-24 p-2 m-2" loading='lazy' />
-                    </a>
+        <section className="bg-gray-800 pt-12">
+            <div className="container px-6 py-12 mx-auto">
+                <div className="text-center">
+                    <p className="flex items-center justify-center text-4xl font-noto-sans font-bold w-auto m-8 bg-clip-text text-transparent bg-gradient-to-r from-lime-400 to-lime-100 sm:text-6xl sm:leading-normal">Contacto</p>
+
+                    <p className="mt-3 text-gray-500">Siéntete libre de contactarme para cualquier consulta.</p>
                 </div>
-                ))}
+
+                <div className="grid grid-cols-1 gap-12 mt-10 md:grid-cols-2 lg:grid-cols-3">
+                    <div className="flex flex-col items-center justify-center text-center">
+                        <span className="p-3 text-blue-500 rounded-full bg-blue-100/80 dark:bg-gray-800">
+                            <img src={github} alt='github' className="w-6 h-6" />
+                        </span>
+                        <h2 className="mt-4 text-lg font-medium text-gray-800 dark:text-white">Github</h2>
+                        <p className="mt-2 text-gray-500 dark:text-gray-400">Explora mis proyectos y contribuciones en GitHub.</p>
+                        <a href='https://github.com/Rodrigo-Lajas' className="mt-2 text-blue-500 dark:text-blue-400">Mi perfil de GitHub</a>
+                    </div>
+
+                    <div className="flex flex-col items-center justify-center text-center">
+                        <span className="p-3 text-blue-500 rounded-full bg-blue-100/80 dark:bg-gray-800">
+                            <img src={linkedin} alt='linkedin' className="w-6 h-6" />
+                        </span>
+
+                        <h2 className="mt-4 text-lg font-medium text-gray-800 dark:text-white">Linkedin</h2>
+                        <p className="mt-2 text-gray-500 dark:text-gray-400">Conéctate para conocer mi experiencia profesional.</p>
+                        <a href='https://www.linkedin.com/in/rodrigolajas/' className="mt-2 text-blue-500 dark:text-blue-400">Mi perfil de Linkedin</a>
+                    </div>
+
+                    <div className="flex flex-col items-center justify-center text-center">
+                        <span className="p-3 text-blue-500 rounded-full bg-blue-100/80 dark:bg-gray-800">
+                            <img src={mail} alt='mail' className="w-6 h-6" />
+                        </span>
+
+                        <h2 className="mt-4 text-lg font-medium text-gray-800 dark:text-white">Mail</h2>
+                        <p className="mt-2 text-gray-500 dark:text-gray-400">Contáctame para cualquier consulta o colaboración.</p>
+                        <p className="mt-2 text-blue-500 dark:text-blue-400">Envíame un correo</p>
+                    </div>
+                </div>
             </div>
-        </section >
+        </section>
     );
 }
