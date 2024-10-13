0fa9de1b06a0343cf46c2a3711f4406c8cba6b14
Rodrigo-Lajas <rodrigolajas18@gmail.com>
2024-10-13 15:43:58 -0300

    implemento de react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,26 @@ import Navbar from "./components/NavBar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Education from "./components/Education";
-// import Projects from "./components/Projects";
+import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 export default function App() {
     return (
         <div className="flex flex-col h-screen font-sans">
-            <Navbar />
-            <Hero />
-            <About />
-            <Education />
-            {/* <Projects /> */}
-            <Contact />
-            <Footer />
-            <SpeedInsights />
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Hero />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/education" element={<Education />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+                <Footer />
+                <SpeedInsights />
+            </BrowserRouter>
         </div>
     );
 }
